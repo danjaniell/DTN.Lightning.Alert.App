f33d7f99fa04b55d6061a47adad544c5e70074a5
Jai <iaj.paragas@yahoo.com>
2022-03-23 09:48:29 +0800

    Convert Lat/Lon to QuadKey service

---
+++ src/Services/CoordinatesService.cs
@@ -0,0 +1,16 @@
+﻿namespace DTN.Lightning.Alert.App.Services
+{
+    public static class CoordinatesService
+    {
+        private const int mapZoom = 12;
+
+        public static string LatLonToQuadKey(double latitude, double longitude)
+        {
+            TileSystem.LatLongToPixelXY(latitude, longitude, mapZoom, out int pixelX, out int pixelY);
+            TileSystem.PixelXYToTileXY(pixelX, pixelY, out int tileX, out int tileY);
+            string quadKey = TileSystem.TileXYToQuadKey(tileX, tileY, mapZoom);
+
+            return quadKey;
+        }
+    }
+}
